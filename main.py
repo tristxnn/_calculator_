8938a5f047f60d9d38633c2177173a569d352d9f
tristxnn <tristanguire@gmail.com>
2022-03-02 13:17:21 -0600

    Initial Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Welcome to the BMI Calculator!")
+print("")
+weight = input("What is your weight (lbs)?: ")
+print("")
+height = input("Great! Now what is your height (ft)?: ")
+
+height = float(height)
+weight = int(weight)
+
+lbsConversion = (weight * 0.4535924)
+ftConversion = (height * 0.3048)
+
+bmi = int(lbsConversion / float(ftConversion * ftConversion))
+round(bmi, 2)
+print("")
+print(f"Your BMI is {bmi}")
