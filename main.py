e40af972bdb69d02071f5ade1df7976232f1b3ce
tristxnn <tristanguire@gmail.com>
2022-03-02 16:47:26 -0600

    Initial Commit

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-print("Welcome to the BMI Calculator!")
+print("Welcome to the ticket booth!")
 print("")
-weight = input("What is your weight (lbs)?: ")
-print("")
-height = input("Great! Now what is your height (ft)?: ")
+height = int(input("What is your height in cm?: " ))
+bill = 0
 
-height = float(height)
-weight = int(weight)
+if height >= 120:
+    print("You can ride the rollercoaster!")
+    print("")
+    age = int(input("What is your age?: " ))
+    if age < 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
 
-lbsConversion = (weight * 0.4535924)
-ftConversion = (height * 0.3048)
+    photo = input("Would you like a photo taken? Y or N. ")
+    if photo == "Y" or "y":
+        bill += 3
 
-bmi = int(lbsConversion / float(ftConversion * ftConversion))
-round(bmi, 2)
-print("")
-print(f"Your BMI is {bmi}")
-print("")
+    print(f"Your ticket cost is {bill}")
 
-input("     Press enter to exit!")
+else:
+    print("Sorry! You do not meet the height requirement for this ride.")
+
