28981e5c7c844a13402fa2ba888768ca7110acfb
Tristan <tristanguire@gmail.com>
2022-03-16 18:54:57 -0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print("Welcome to the basic calculator!")
 print("")
 cont = input("Would you like to continue? Y/N: ").lower()
-if cont == "y":
+while cont == "y":
     first = input("Great! What is your first number?: ")
     second = input("Awesome! What is your second number?: ")
     op = input("What operator would you like to use? +, -, *, / ?: ")
@@ -17,11 +17,14 @@ if cont == "y":
     elif op == "/":
         divide = int(first) / int(second)
         print(f"{first} / {second} = {divide}!")
-    else:
-        print("Invalid!")
 
-elif cont == "n":
-    quit()
-else:
-    print("That is an invalid answer!")
-    quit()
+    q = input('do you want to continue?: ').lower()
+    if q == 'y':
+        continue
+    elif q == 'n':
+        print("Okay! Goodbye!")
+        break
+    else:
+        print("Invalid answer!")
+        break
+
